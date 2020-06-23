c72327699f0680019caa63d79f5a66951cba33ec
Sebba8 <47074056+Sebby37@users.noreply.github.com>
2020-06-23 10:57:51 +0930

    Update ConsoleSecret.js

--- ConsoleSecret.js
+++ ConsoleSecret.js
@@ -1,4 +1,4 @@
-function secretEasterEgg!hkjlsgfdsghkdfjlpwhiuegr()
+function secretEasterEgghkjlsgfdsghkdfjlpwhiuegr()
 {
     throw("HAHA, PRANKED! YOU REALLY THOUGHT I WOULD HAVE PREPARED SOMETHING COOL/USEFUL! exception");
     while (true)
@@ -11,4 +11,4 @@ console.log("Hello there, you seem to have stumbled upon the console area of my 
 console.log("There isn't much to say here to be honest.");
 console.log("However there is a secred b@se 85 function, decrypt this and run it in the console.");
 console.log("You won't be dissapointed...")
-console.log("F(Jj%ATU@LF*(u67;$6VBPV@!F(])qF(]0%A7fRmE-Gu0F_,3+-n.4");
+console.log("F(Jj%ATU@LF*(u67;$7HCMI[,B4b[qB4u'kAnl.$GA(]5AS6$3.6@");
