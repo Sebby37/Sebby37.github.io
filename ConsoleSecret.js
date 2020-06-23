30b811b947a9e190900658cc02fdf0a067c39bfd
Sebba8 <47074056+Sebby37@users.noreply.github.com>
2020-06-23 11:07:23 +0930

    Update ConsoleSecret.js

--- ConsoleSecret.js
+++ ConsoleSecret.js
@@ -11,4 +11,5 @@ console.log("Hello there, you seem to have stumbled upon the console area of my 
 console.log("There isn't much to say here to be honest.");
 console.log("However there is a secred b@se 85 function, decrypt this and run it in the console.");
 console.log("You won't be dissapointed...");
+console.log("Or maybe you will, anyway decode the below phrase for a secret:");
 console.log("F(Jj%ATU@LF*(u67;$7HCMI[,B4b[qB4u'kAnl.$GA(]5AS6$3.6@");
