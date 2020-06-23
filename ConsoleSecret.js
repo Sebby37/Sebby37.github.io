8f5f138cd93c93318fe10fab9d7890f35bc5c633
Sebba8 <47074056+Sebby37@users.noreply.github.com>
2020-06-23 10:55:12 +0930

    Nothing out of the ordinary here...

---
+++ ConsoleSecret.js
@@ -0,0 +1,14 @@
+function secretEasterEgg!hkjlsgfdsghkdfjlpwhiuegr()
+{
+    throw("HAHA, PRANKED! YOU REALLY THOUGHT I WOULD HAVE PREPARED SOMETHING COOL/USEFUL! exception");
+    while (true)
+        {
+            console.log("Haha pranked!");
+        }
+}
+throw("Easter egg hunter exception");
+console.log("Hello there, you seem to have stumbled upon the console area of my site!");
+console.log("There isn't much to say here to be honest.");
+console.log("However there is a secred b@se 85 function, decrypt this and run it in the console.");
+console.log("You won't be dissapointed...")
+console.log("F(Jj%ATU@LF*(u67;$6VBPV@!F(])qF(]0%A7fRmE-Gu0F_,3+-n.4");
