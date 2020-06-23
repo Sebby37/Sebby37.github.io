9e3ade1ecc56c2bcda4908a52291684c4f40c933
Sebba8 <47074056+Sebby37@users.noreply.github.com>
2020-06-23 10:58:52 +0930

    Update ConsoleSecret.js

--- ConsoleSecret.js
+++ ConsoleSecret.js
@@ -10,5 +10,5 @@ throw("Easter egg hunter exception");
 console.log("Hello there, you seem to have stumbled upon the console area of my site!");
 console.log("There isn't much to say here to be honest.");
 console.log("However there is a secred b@se 85 function, decrypt this and run it in the console.");
-console.log("You won't be dissapointed...")
+console.log("You won't be dissapointed...");
 console.log("F(Jj%ATU@LF*(u67;$7HCMI[,B4b[qB4u'kAnl.$GA(]5AS6$3.6@");
